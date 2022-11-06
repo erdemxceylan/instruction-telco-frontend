aa9cf5b304cfca6601022d29e9a5608da3cc831f
Erdem Ceylan <erdemxceylan@gmail.com>
2022-11-06 09:02:43 +0300

    service list crud logic has been completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { CreateFakeArrayPipe } from './pipes/create-fake-array.pipe';
 import { HomepageComponent } from './pages/homepage/homepage.component';
-import { ListviewComponent } from './components/listview/listview.component';
+import { LoadingComponent } from './components/loading/loading.component';
 import { LoadingInterceptor } from './interceptors/loading.interceptor';
 import { LoginComponent } from './pages/login/login.component';
 import { NgModule } from '@angular/core';
+import { ServiceListComponent } from './components/service-list/service-list.component';
 import { SplitPipe } from './pipes/split.pipe';
-import { LoadingComponent } from './components/loading/loading.component';
 
 @NgModule({
-   declarations: [AppComponent, ListviewComponent, CreateFakeArrayPipe, SplitPipe, LoginComponent, HomepageComponent, LoadingComponent],
+   declarations: [AppComponent, ServiceListComponent, CreateFakeArrayPipe, SplitPipe, LoginComponent, HomepageComponent, LoadingComponent],
    imports: [
       BrowserModule,
       AppRoutingModule,
---
+++ src/app/components/service-list/service-list.component.ts
@@ -0,0 +1,118 @@
+import { Component, OnInit } from '@angular/core';
+import {
+   FormBuilder,
+   FormGroup,
+   Validators,
+} from '@angular/forms';
+
+import { Service } from 'src/app/models/service';
+import { ServicesService } from '../../services/services.service';
+
+@Component({
+   selector: 'app-service-list',
+   templateUrl: './service-list.component.html',
+   styleUrls: ['./service-list.component.css'],
+})
+export class ServiceListComponent implements OnInit {
+   services!: Service[];
+   addForm!: FormGroup;
+   addError: string = '';
+   editForm!: FormGroup;
+   editError: string = '';
+   isEditing: boolean = false;
+   editId!: number;
+
+   constructor(
+      private servicesService: ServicesService,
+      private formBuilder: FormBuilder
+   ) { }
+
+   ngOnInit(): void {
+      this.getServices();
+      this.createForms();
+   }
+
+   createForms() {
+      this.addForm = this.formBuilder.group({
+         name: ['', Validators.required],
+      });
+      this.editForm = this.formBuilder.group({
+         name: ['', Validators.required],
+      });
+   }
+
+   getServices(): void {
+      this.servicesService.getServices().subscribe((response) => {
+         this.services = response;
+      });
+   }
+
+   add(): void {
+      if (this.addForm.invalid) {
+         this.addError = 'Form is invalid';
+         return;
+      }
+      if (this.addError) this.addError = '';
+
+      const service: Service = {
+         ...this.addForm.value,
+      };
+
+      this.servicesService.add(service).subscribe({
+         next: () => {
+            console.info(`${service.name} has been added.`);
+         },
+         error: (err) => {
+            console.log(err);
+            this.addError = err.statusText;
+         },
+         complete: () => {
+            if (this.addError) this.addError = '';
+            this.addForm.reset();
+            this.getServices();
+         },
+      });
+   }
+
+   edit(id: number) {
+      this.isEditing = true;
+      this.editId = id;
+      this.editForm.get('name')?.setValue(this.services.find(service => service.id === id)?.name);
+   }
+
+   update(): void {
+      if (this.editForm.invalid || !this.editId) {
+         this.editError = 'Form is invalid';
+         return;
+      }
+      if (this.editError) this.editError = '';
+
+      const service: Service = {
+         id: this.editId,
+         ...this.editForm.value,
+      };
+
+      this.servicesService.update(service).subscribe({
+         next: () => {
+            console.info('İşlem başarılı');
+         },
+         error: (err) => {
+            console.log(err);
+            this.editError = err.statusText;
+         },
+         complete: () => {
+            if (this.editError) this.editError = '';
+            this.editForm.reset();
+            this.getServices();
+         },
+      });
+
+      this.isEditing = false;
+   }
+
+   delete(id: number) {
+      this.servicesService.delete(id).subscribe(() => {
+         this.getServices();
+      });
+   }
+}
